ac32ef76c657dbe514408a5ee2d40600fef43702
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2024-01-23 12:09:45 +0000

    Get parts

--- js/parts.js
+++ js/parts.js
@@ -1,11 +1,43 @@
 const url = 'http://localhost:3000/parts';
 
+// prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+
 async function getParts() {
 	const res = await fetch(url);
 
 	const parts = await res.json();
 
-	console.log('Parts: ', parts);
+	for (const article of parts) {
+		let allEment = [];
+		const imgElement = document.createElement('img');
+		imgElement.src = article.image;
+		allEment.push(imgElement);
+
+		const titleElement = document.createElement('h2');
+		titleElement.innerText = article.nom;
+		allEment.push(titleElement);
+
+		const priceElement = document.createElement('p');
+		priceElement.innerText = `Prix: ${article.prix} + €`;
+		allEment.push(priceElement);
+
+		const categoryElement = document.createElement('p');
+		categoryElement.innerText = article.categorie;
+		allEment.push(categoryElement);
+
+		addChildElement(allEment);
+	}
+}
+
+function addChildElement(elements) {
+	const articleElement = document.createElement('article');
+	const fichesElement = document.querySelector('.fiches');
+
+	for (const element of elements) {
+		articleElement.appendChild(element);
+	}
+
+	fichesElement.appendChild(articleElement);
 }
 
 getParts();
